4d9c016e07e4f205b88f027adc8c5754ab924047
mainlego <vladmelbiz@gmail.com>
2025-12-09 19:55:42 +0200

    Move driver app to /driver/ basePath

--- apps/driver/next.config.js
+++ apps/driver/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  basePath: '/driver',
   reactStrictMode: true,
   transpilePackages: ['@vibe-taxi/database'],
   images: {
